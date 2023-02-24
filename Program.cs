45e85ea1b76bd83468d2e578fc3b37c2376a2169
Denis Filatov <gooden@mail.ru>
2023-02-24 15:01:41 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Task 6; Программа, которая на вход принимает 1 число и выдает, является ли число четным
+Console.WriteLine("Task 6; Программа, которая на вход принимает 1 число и выдает, является ли число четным");
+Console.Write("Введите ваше число: "); 
+int numA = Convert.ToInt32 (Console.ReadLine());
+
+if (numA %2==0)
+{
+     Console.WriteLine($"Число: {numA} является четным");
+}
+else
+{
+     Console.WriteLine($"Число: {numA} является нечетным");
+}
